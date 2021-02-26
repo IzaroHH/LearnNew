1d6fde6c0e38bf336a40b69f0d025ffc396c2940
Izaro <hu670058122@163.com>
2021-02-26 17:08:55 +0800

    Day01

---
+++ src/cn/tedu/hello/HelloWorld.java
@@ -0,0 +1,21 @@
+
+package cn.tedu.hello;
+/**
+ * package --刚刚创建的包
+ * cn.tedu.hello --包名,公司的域名倒着写,全小写,用"."隔开
+ * ; --英文分号,表示一句话的结束
+ */
+
+/**
+ * public class HelloWorld{}
+ * public 公共的,用来修饰谁可以访问这个类
+ * class --这是一个类
+ * HelloWorld --给这个类起的名字,驼峰命名法,每个单词的首字母大写
+ * {类的主题内容} --用来放置类的主要内容
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("元宵节快乐!");
+        System.out.println("HelloWorld!");
+    }
+}
